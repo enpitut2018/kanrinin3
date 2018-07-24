f51def21454ca45d3ccbc51c4b149fc4e9d921d1
yusa <l.r.play.gvig.wav@gmail.com>
2018-07-24 16:59:20 +0900

    icsファイル形式に対応（しつつある）

--- piyopiyo.py
+++ piyopiyo.py
@@ -1,10 +1,12 @@
+import re
+
 class Ikkokukan():
     def __init__(self):
         self.free_list = []
         self.isogashi_list = []
         self.isEnd = False
         
-    def schedule_add(self,date):
+    def schedule_free_add(self,date):
         if(date.isdecimal()):
             num = int(date)
             if(num<1 or num>31):
@@ -12,8 +14,12 @@ class Ikkokukan():
             else:
                 self.free_list.append(num)
 
-    def schedule_show(self):
+    def schedule_free_show(self):
         print(self.free_list)
+
+
+path_kyoko = 'Kyokosan.ics'
+path_godai = 'Godaisan.ics'
 
 kyokosan = Ikkokukan()
 godaisan = Ikkokukan()
@@ -23,20 +29,44 @@ isMatch = False
 inviteEnd = False
 day = 1
 
+with open(path_kyoko) as f:
+    lines = f.readlines()
+
+lines_strip = [line.strip() for line in lines]
+
+l_DTSTART = [line for line in lines_strip if 'DTSTART' in line]
+l_DTEND = [line for line in lines_strip if 'DTSTART' in line]
+i=0
+while i < len(l_DTSTART):
+    matchObj = re.search(r'[0-9]{8}', l_DTSTART[i])
+    print("START: " + matchObj.group())
+    matchObj = re.search(r'[0-9]{8}', l_DTEND[i])
+    print("END: " + matchObj.group())
+    i += 1
+#print(l_DTSTART)
+
+with open(path_godai) as f:
+    lines = f.readlines()
+
+lines_strip = [line.strip() for line in lines]
+
+l_DTSTART = [line for line in lines_strip if 'DTSTART' in line]
+#print(l_DTSTART)
+
+'''
 kyokosan.isogashi_list = [1,2,4,7,8,11,12,14,16,17,19,20,22,24,27,30]
 godaisan.isogashi_list = [2,3,5,7,9,11,13,14,15,18,19,24,26,28,30,31]
 
-'''
 print("二人の日程を入力してください．終了する場合は0を入力してください．")
 
 print("響子さんの空いてる日is:")
 while kyokosan.isEnd == False:
-    kyokosan.schedule_add(input())
+    kyokosan.schedule_free_add(input())
 
 print("五代さんの空いてる日is:")
 while godaisan.isEnd == False:
-    godaisan.schedule_add(input())
-'''
+    godaisan.schedule_free_add(input())
+
 
 while day < 32:
     if day not in kyokosan.isogashi_list:
@@ -48,10 +78,10 @@ while day < 32:
 kyokosan.free_list.sort()
 godaisan.free_list.sort()
 
-print("echo: 響子さんの空いてる日is:")
-kyokosan.schedule_show()
-print("echo: 五代さんの空いてる日is:")
-godaisan.schedule_show()
+print("響子さんの空いてる日is:")
+kyokosan.schedule_free_show()
+print("五代さんの空いてる日is:")
+godaisan.schedule_free_show()
 
 matched_list = []
 for kyoko in kyokosan.free_list:
@@ -76,3 +106,4 @@ if isMatch:
 
 else:
     print("来月は頑張ってください...")
+'''
