fbe3dcb9fb7e1aa5c1006faa488c9d360ba190d1
yusa <l.r.play.gvig.wav@gmail.com>
2018-07-26 18:08:07 +0900

    コード整理

--- piyopiyo.py
+++ piyopiyo.py
@@ -37,10 +37,6 @@ file_name1 = 'data/Kyokosan.ics'
 path_kyoko = file_name1
 #path_godai = file_name2
 
-#urllib.request.urlretrieve(URL1, file_name1)
-#urllib.request.urlretrieve(URL2, file_name2)
-
-
 class Ikkokukan():
     def __init__(self):
         self.free_set = set() #暇な日を格納するset
@@ -139,8 +135,6 @@ class Ikkokukan():
             i += 1
 
 #フラグ
-isMatch = False
-inviteEnd = False
 byebye_flag = False
 
 
@@ -150,49 +144,17 @@ while calc_date <= boshu_end:
     everyone_free_set.add(calc_date)
     calc_date = calc_date + datetime.timedelta(days=1)
 
-'''
-#響子さんのカレンダー読み込み
-#todo 響子さんのも合わせて関数化する
-with open(path_kyoko) as f:
-    lines = f.readlines()
-
-lines_strip = [line.strip() for line in lines]
-
-l_DTSTART = [line for line in lines_strip if 'DTSTART' in line]
-l_DTEND = [line for line in lines_strip if 'DTEND' in line]
-i=0
-while i < len(l_DTSTART):
-    #予定開始日
-    matchObj_start = re.search(r'[0-9]{8}', l_DTSTART[i])
-    #日付型に変換
-    date_formatted_start = datetime.datetime.strptime(matchObj_start.group(), "%Y%m%d")
-    #print("start: ", end="")
-    #print(date_formatted_start.date())
-
-    #予定終了日
-    matchObj_end = re.search(r'[0-9]{8}', l_DTEND[i])
-    #日付型に変換
-    date_formatted_end = datetime.datetime.strptime(matchObj_end.group(), "%Y%m%d")
-    #print('end  : ', end="")
-    #print(date_formatted_end.date())
-
-    #予定のある日を計算
-    calc_date = date_formatted_start
-    while calc_date < date_formatted_end:
-        kyokosan.schedule_busy_add(calc_date)
-        calc_date = calc_date + datetime.timedelta(days=1)
-
-    i += 1
-'''
 
 while byebye_flag == False:
 
     #インスタンス作成
     kyokosan = Ikkokukan()
-
-    if kyokosan.listen_url():
+    print('URL?:')
+    input_str = input()
+    if input_str == 'end':
         byebye_flag = True
     else:
+        kyokosan.set_url(input_str)
         kyokosan.run()
         
         #print("響子さんの忙しい日リスト:")
@@ -219,60 +181,3 @@ while i < len(free_list):
     print(str(free_list[i].day))
     i += 1
 print("Have a nice day!")
-
-'''
-while boshu_start.date() < boshu_end.date():
-    tmp = boshu_start
-    while tmp.date() < boshu_end.date()
-        if tmp.date() not in kyokosan.busy_set[0].date():
-            kyokosan.free_set.append(boshu_start)
-        #todo:五代さんの分も
-        tmp = tmp + datetime.timedelta(days=1)
-    boshu_start = boshu_start + datetime.timedelta(days=1)
-'''
-
-'''
-print("響子さんの忙しい日is:")
-kyokosan.schedule_busy_show()
-print("惣一郎さんが暇な日is:")
-soichirosan.schedule_free_show()
-print("響子さんの空いてる日is:")
-kyokosan.schedule_free_show()
-'''
-
-'''
-#暇な日print
-print("響子さんの空いてる日is:")
-kyokosan.schedule_free_show()
-print("五代さんの空いてる日is:")
-godaisan.schedule_free_show()
-
-
-#暇な日マッチング
-matched_set = set()
-for kyoko in kyokosan.free_set:
-    for godai in godaisan.free_set:
-        if kyoko == godai:
-            matched_set.append(kyoko)
-            isMatch = True
-
-#マッチする日があった場合
-if isMatch:
-    sorted_set = set(set(matched_set))
-    sorted_set.sort()
-    print(" 二人の空いてる日is:")
-    print(sorted_set)
-    while inviteEnd == False:
-        print("招待する日を選択してください:")
-        inviteDay = input()
-        if int(inviteDay) in sorted_set:
-            print(inviteDay + "日に招待を送りました!!!!!!!!")
-            inviteEnd = True
-        else:
-            print(inviteDay + "日は予定があるみたいです...")
-
-#マッチする日がなかった場合
-else:
-    print("来月は頑張ってください...")
-    
-'''
