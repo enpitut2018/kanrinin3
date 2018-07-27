02cb40027cdde90ac1ff0e529cbad153efa555ad
yusa <l.r.play.gvig.wav@gmail.com>
2018-07-27 14:28:16 +0900

    startの直後にendした時のエラーメッセージ追加

--- slackbot_settings.py
+++ slackbot_settings.py
@@ -1,7 +1,8 @@
 # coding: utf-8
 
 # botアカウントのトークンを指定
-API_TOKEN = "xoxb-404455848946-405776099334-5rDsuTqtM8ImDbGNwWa2tw4e"
+#API_TOKEN = "xoxb-404455848946-405776099334-5rDsuTqtM8ImDbGNwWa2tw4e"
+API_TOKEN = "xoxb-332729453684-407267663062-tywbBsNfGIPsthnJ4vmyf5Qp"
 
 # このbot宛のメッセージで、どの応答にも当てはまらない場合の応答文字列
 DEFAULT_REPLY = "何言ってんだこいつ"
