e4cdfd8fdb2aa4de8a16a8a5b12604cc37d92f2f
yusa <l.r.play.gvig.wav@gmail.com>
2018-07-30 09:11:31 +0900

    dbshow修正

--- plugins/my_mention.py
+++ plugins/my_mention.py
@@ -156,7 +156,7 @@ def showdb_funk(message):
     global database_path
     with open(database_path, 'r') as f:
         for line in f.readlines():
-            matchObj = re.search(r'\$.+\s')
+            matchObj = re.search(r'\$.+\s', line)
                 message.send(str(matchObj.group()))
 
 @respond_to(r'^help$')
